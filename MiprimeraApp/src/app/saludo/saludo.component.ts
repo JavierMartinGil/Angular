ad661bb71145bfbe8682ee6df4187e4b6cbfb93f
javier_martin83 <javier_martin83@yahoo.es>
2019-07-29 17:23:21 +0200

    Primera app Angular

---
+++ MiprimeraApp/src/app/saludo/saludo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+
+  mensaje: string;
+
+  constructor() {
+    this.mensaje = 'Este es mi mensaje de saludo';
+  }
+
+  ngOnInit() {
+  }
+
+}
