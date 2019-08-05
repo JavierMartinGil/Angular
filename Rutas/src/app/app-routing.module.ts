1fcd34e47d899697aa57d7bbf2950d5bfe55b16a
javier_martin83 <javier_martin83@yahoo.es>
2019-08-05 10:59:25 +0200

    Angular routing

--- Rutas/src/app/app-routing.module.ts
+++ Rutas/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'info', redirectTo: 'contact' },
+  { path: 'contact', component: ContactComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: '**', redirectTo: 'main' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Rutas/src/app/app.module.ts
+++ Rutas/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContactComponent,
+    AboutComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
